76e672e6111b9a9506de2cfa00c8b1bb2620faa7
StarPandaBeg <kshmurakov@mail.ru>
2024-04-26 18:45:10 +0300

    Feature: Add dependencies
    Install & configure Vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,31 @@
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+    "@pinia/nuxt",
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.API_BASE_URL,
+    },
+    postgresConnectionUrl: process.env.POSTGRES_URL,
+  },
+});
