19125eddb081a75f9a8f29984bbfd2276ea136b8
StarPandaBeg <kshmurakov@mail.ru>
2024-06-04 01:31:30 +0300

    fix: explicit cors header set

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,8 +1,9 @@
 export default defineEventHandler((event) => {
   const headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "*",
-    "Access-Control-Allow-Credentials": "*",
+    "Access-Control-Allow-Origin": "http://localhost:3000",
+    "Access-Control-Allow-Headers":
+      "origin, content-type, accept, authorization",
+    "Access-Control-Allow-Credentials": "true",
   };
   // setHeaders(event, headers);
   setResponseHeaders(event, headers);
